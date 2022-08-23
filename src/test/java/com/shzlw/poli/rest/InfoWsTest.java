b66b6fde3c1d91745f7669628a25ca97c8ca6a3d
Baoqi Wu <wubaoqi@gmail.com>
2022-08-23 19:43:50 +0800

    remove info/general request

--- src/test/java/com/shzlw/poli/rest/InfoWsTest.java
+++ src/test/java/com/shzlw/poli/rest/InfoWsTest.java
@@ -1,11 +1,6 @@
 package com.shzlw.poli.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.shzlw.poli.config.AppProperties;
-import com.shzlw.poli.dto.AppInfo;
-import com.shzlw.poli.util.Constants;
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,10 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -30,17 +21,4 @@ public class InfoWsTest {
     @Autowired
     ObjectMapper mapper;
 
-    @Autowired
-    AppProperties appProperties;
-
-    @Test
-    public void testGetAppInfo() throws Exception {
-        MvcResult mvcResult = mvc.perform(get("/info/general")).andExpect(status().isOk())
-                .andReturn();
-        String responeText = mvcResult.getResponse().getContentAsString();
-        AppInfo appInfo = mapper.readValue(responeText, AppInfo.class);
-
-        Assert.assertEquals(Constants.CURRENT_VERSION, appInfo.getVersion());
-        Assert.assertEquals(appProperties.getLocaleLanguage(), appInfo.getLocaleLanguage());
-    }
 }
