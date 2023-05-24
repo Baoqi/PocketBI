1e8685f8259032a3f6bb218ce23ad42220af4eb7
Baoqi Wu <wubaoqi@gmail.com>
2023-05-24 19:41:47 +0800

    move select to antd

--- web-app/src/components/Select.js
+++ web-app/src/components/Select.js
@@ -1,72 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Select as AntdSelect } from 'antd';
 
-class Select extends React.Component {
+function Select(props) {
+  const {
+    name,
+    value,
+    options = [],
+    optionValue,
+    optionDisplay,
+    onChange,
+    preloadOneEmpty = true
+  } = props;
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    options: PropTypes.array.isRequired,
-    optionDisplay: PropTypes.string,
-    optionValue: PropTypes.string,
-    preloadOneEmpty: PropTypes.bool
-  };
-
-  handleOptionChange = (event) => {
-    const name = this.props.name;
-    const value = event.target.value;
-    this.props.onChange(name, value);
-  }
-  
-  render() {
-    const {
-      value,
-      options = [],
-      optionValue,
-      optionDisplay,
-      preloadOneEmpty = true
-    } = this.props;
-
-    const optionList = [];
-    if (preloadOneEmpty) {
-      optionList.push(
-        <option value="" key=""></option>
-      );
-    }
-    
-    options.forEach((option, index) => {
-      let value;
-      let display;
-      if (optionValue && optionDisplay) {
-        // The options contain objects.
-        value = option[optionValue];
-        display = option[optionDisplay];
-      } else {
-        // The options contain string or number.
-        value = option;
-        display = option;
-      }
-      
-      optionList.push(
-        <option value={value} key={value}>{display}</option>
-      ) 
+  const optionList = [];
+  if (preloadOneEmpty) {
+    optionList.push({
+      value: '',
+      label: ''
     });
+  }
+    
+  options.forEach((option, index) => {
+    let value;
+    let display;
+    if (optionValue && optionDisplay) {
+      // The options contain objects.
+      value = option[optionValue];
+      display = option[optionDisplay];
+    } else {
+      // The options contain string or number.
+      value = option;
+      display = option;
+    }
 
-    return (
-      <select 
-        className="form-input"
-        value={value} 
-        onChange={this.handleOptionChange}>
-        {optionList}
-      </select>
-    )
-  };
+    optionList.push({
+      value: value,
+      label: display
+    });
+  });
+
+  return (
+      <AntdSelect
+          showSearch
+          value={value}
+          size={'large'}
+          style={{ width: '100%' }}
+          onChange={v => onChange(name, v)}
+          filterOption={(input, option) =>
+              (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+          }
+          options = {optionList}
+      />
+  );
 }
+
+Select.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired,
+  optionDisplay: PropTypes.string,
+  optionValue: PropTypes.string,
+  preloadOneEmpty: PropTypes.bool
+};
 
 export default Select;
