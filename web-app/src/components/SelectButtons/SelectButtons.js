f36269ae8ad7bc0eb1a368a39fd278f2ccdc3531
Baoqi Wu <wubaoqi@gmail.com>
2023-05-24 15:10:40 +0800

    Move SelectButton to function style

--- web-app/src/components/SelectButtons/SelectButtons.js
+++ web-app/src/components/SelectButtons/SelectButtons.js
@@ -2,48 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './SelectButtons.css';
 
-class SelectButtons extends React.Component {
+function SelectButtons(props) {
+  const {
+    name,
+    value,
+    selections = [],
+    onChange
+  } = props;
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    selections: PropTypes.array.isRequired,
-    onChange: PropTypes.func.isRequired
-  };
-
-  clickButton = (value) => {
-    const { name } = this.props;
-    this.props.onChange(name, value);
-  }
-  
-  render() {
+  const buttonItems = [];
+  for (let i = 0; i < selections.length; i++) {
     const {
-      value,
-      selections = []
-    } =  this.props;
-    const buttonItems = [];
-    for (let i = 0; i < selections.length; i++) {
-      const {
-        display,
-        value: actualValue
-      } = selections[i];
-      const activeClass = actualValue === value ? 'select-button-active' : '';
-      buttonItems.push(
-        <div className={`select-button ${activeClass}`} onClick={() => this.clickButton(actualValue)} key={actualValue}>{display}</div>
-      )
-    }
+      display,
+      value: actualValue
+    } = selections[i];
+    const activeClass = actualValue === value ? 'select-button-active' : '';
+    buttonItems.push(
+        <div className={`select-button ${activeClass}`} onClick={() => onChange(name, actualValue)} key={actualValue}>{display}</div>
+    )
+  }
 
-    return (
+  return (
       <div className="selectbuttons-container">
         {buttonItems}
       </div>
-    )
-  };
+  );
 }
+
+SelectButtons.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  selections: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired
+};
 
 export default SelectButtons;
