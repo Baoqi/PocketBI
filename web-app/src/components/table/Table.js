e584ff37678526c037048baa9bbf698cb5753c89
Baoqi Wu <wubaoqi@gmail.com>
2023-05-24 14:18:08 +0800

    move Table to antd

--- web-app/src/components/table/Table.js
+++ web-app/src/components/table/Table.js
@@ -1,89 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactTable from 'react-table';
-import 'react-table/react-table.css';
-import './Table.css';
+import { Table as AntdTable } from 'antd';
 
-class Table extends React.PureComponent {
+function Table(props) {
+  const {
+    data = [],
+    columns = [],
+    drillThrough = [],
+    defaultPageSize = 10,
+    showPagination = true,
+    errorMsg,
+    onTableTdClick
+  } = props;
 
-  static propTypes = {
-    data: PropTypes.array.isRequired,
-    columns: PropTypes.array.isRequired,
-    defaultPageSize: PropTypes.number,
-    drillThrough: PropTypes.array,
-    showPagination: PropTypes.bool,
-    height: PropTypes.number,
-    errorMsg: PropTypes.string
-  };
-
-  handleTdClick = (reportId, columnName, columnValue) => {
-    this.props.onTableTdClick(reportId, columnName, columnValue);
+  const handleTdClick = (reportId, columnName, columnValue) => {
+    onTableTdClick(reportId, columnName, columnValue);
   }
 
-  render() {   
-    const {
-      data = [],
-      columns = [],
-      drillThrough = [],
-      defaultPageSize = 10,
-      showPagination = true,
-      height,
-      errorMsg
-    } = this.props;
-
-    const columnHeaders = [];
-    columns.forEach(column => {
-      const columnName = column.name;
-      const header = {
-        Header: columnName,
-        accessor: columnName
-      };
-      if (drillThrough?.length > 0) {
-        const index = drillThrough.findIndex(d => d.columnName === columnName);
-        if (index !== -1) {
-          const reportId = drillThrough[index].reportId;
-          header.Cell = (props => 
-            <span className="link-label" 
-              onClick={() => this.handleTdClick(reportId, columnName, props.value)}>
-              {props.value}
-            </span>
-          );
-        }
+  const columnHeaders = [];
+  columns.forEach(column => {
+    const columnName = column.name;
+    const header = {
+      title: columnName.split('_').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' '),
+      dataIndex: columnName,
+      key: columnName,
+      ellipsis: true
+    };
+    if (drillThrough?.length > 0) {
+      const index = drillThrough.findIndex(d => d.columnName === columnName);
+      if (index !== -1) {
+        const reportId = drillThrough[index].reportId;
+        header.render = (text =>
+                <span className="link-label"
+                      onClick={() => handleTdClick(reportId, columnName, text)}>
+                  {text}
+                </span>
+        );
       }
-
-      columnHeaders.push(header);
-    });
-
-    if (errorMsg) {
-      return (
-        <div>{errorMsg}</div>
-      );
     }
 
-    if (data?.length === 0 || columns?.length === 0) {
-      return (
-        <div style={{padding: '10px'}}>No data</div>
-      );
-    }
+    columnHeaders.push(header);
+  });
 
-    const pageSize = showPagination ? undefined : data?.length;
-    const style = height ? {
-      height: height + 'px'
-    } : {};
-    
+  if (errorMsg) {
     return (
-      <ReactTable
-        data={data}
-        columns={columnHeaders}
-        showPagination={showPagination}
-        defaultPageSize={Number(defaultPageSize)}
-        pageSize={pageSize}
-        previousText={'Prev'}
-        nextText={'Next'}
-        style={style}
-      />
+        <div>{errorMsg}</div>
     );
   }
+
+  return (
+      <AntdTable
+          dataSource={data}
+          columns={columnHeaders}
+          size='small'
+          pagination={showPagination && data?.length > Number(defaultPageSize) ? {pageSize: Number(defaultPageSize)} : false}
+      />);
 }
+
+Table.propTypes = {
+  data: PropTypes.array.isRequired,
+  columns: PropTypes.array.isRequired,
+  defaultPageSize: PropTypes.number,
+  drillThrough: PropTypes.array,
+  showPagination: PropTypes.bool,
+  height: PropTypes.number,
+  errorMsg: PropTypes.string
+};
 
 export default Table;
